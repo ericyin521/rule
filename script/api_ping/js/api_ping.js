45e1ca296fcaa9b7a8b453f5cf6cb802151ea3a0
K <704496037@qq.com>
2025-02-12 23:38:32 +0800

    .

--- script/api_ping/js/api_ping.js
+++ script/api_ping/js/api_ping.js
@@ -1,36 +1,35 @@
-// 2025-02-12 23:36:50
+// 2025-02-12 23:38:27
 !(async () => {
-    let timeouts = 5000;
-    try {
-      let url = typeof $request !== "undefined" && $request.url,
-        ms,
-        ins;
-      if (url) {
-        ins = Object.fromEntries(
-          (url.split("?")[1] || "")
-            .split("&")
-            .map((i) => i.split("="))
-            .map(([k, v]) => [k, decodeURIComponent(v)])
-        );
-      }
-  
-      const getEnv = () => {
-        return "undefined" != typeof Egern
-          ? "Egern"
-          : "undefined" != typeof $environment && $environment["surge-version"]
-          ? "Surge"
-          : "undefined" != typeof $environment && $environment["stash-version"]
-          ? "Stash"
-          : "undefined" != typeof module && module.exports
-          ? "Node.js"
-          : "undefined" != typeof $task
-          ? "Quantumult X"
-          : "undefined" != typeof $loon
-          ? "Loon"
-          : "undefined" != typeof $rocket
-          ? "Shadowrocket"
-          : void 0;
-      };
+  let timeouts = 5000;
+  try {
+    let url = typeof $request !== "undefined" && $request.url,
+      ms,
+      ins;
+
+    const getEnv = () => {
+      return "undefined" != typeof Egern
+        ? "Egern"
+        : "undefined" != typeof $environment && $environment["surge-version"]
+        ? "Surge"
+        : "undefined" != typeof $environment && $environment["stash-version"]
+        ? "Stash"
+        : "undefined" != typeof module && module.exports
+        ? "Node.js"
+        : "undefined" != typeof $task
+        ? "Quantumult X"
+        : "undefined" != typeof $loon
+        ? "Loon"
+        : "undefined" != typeof $rocket
+        ? "Shadowrocket"
+        : void 0;
+    };
+    if (url) {
+      ins = Object.fromEntries(
+        (url.split("?")[1] || "")
+          .split("&")
+          .map((i) => i.split("="))
+          .map(([k, v]) => [k, decodeURIComponent(v)])
+      );
       timeouts = Number(ins.timeout) || 5000;
       if (ins.url == "test") {
         ms = "1";
@@ -73,14 +72,27 @@
             });
         });
       }
-      const headers = {
-        "Content-Type": "application/json; charset=utf-8",
-        "Access-Control-Allow-Origin": "*",
-      };
-      if (getEnv() == "Quantumult X") {
-        const s = $environment.version.split(" ");
-        $done({
-          status: "HTTP/1.1 200 OK",
+    }
+    const headers = {
+      "Content-Type": "application/json; charset=utf-8",
+      "Access-Control-Allow-Origin": "*",
+    };
+    if (getEnv() == "Quantumult X") {
+      const s = $environment.version.split(" ");
+      $done({
+        status: "HTTP/1.1 200 OK",
+        headers: headers,
+        body: JSON.stringify({
+          sp: 1,
+          ms: ms,
+          app: getEnv(),
+          timeouts: timeouts,
+        }),
+      });
+    } else {
+      $done({
+        response: {
+          status: 200,
           headers: headers,
           body: JSON.stringify({
             sp: 1,
@@ -88,28 +100,15 @@
             app: getEnv(),
             timeouts: timeouts,
           }),
-        });
-      } else {
-        $done({
-          response: {
-            status: 200,
-            headers: headers,
-            body: JSON.stringify({
-              sp: 1,
-              ms: ms,
-              app: getEnv(),
-              timeouts: timeouts,
-            }),
-          },
-        });
-      }
-    } catch (e) {
-      console.log(e.message);
-      throw new Error();
+        },
+      });
     }
-  })()
-    .catch((e) => {
-      console.log(e.message);
-    })
-    .finally(() => $done());
-  
+  } catch (e) {
+    console.log(e.message);
+    throw new Error();
+  }
+})()
+  .catch((e) => {
+    console.log(e.message);
+  })
+  .finally(() => $done());
